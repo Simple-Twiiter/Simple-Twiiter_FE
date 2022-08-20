24574f515ba502e0a0a756c56cefe7ff00e5416f
DESKTOP-HCK0909\ggg71 <ggg7152@gmail.com>
2022-08-20 23:34:08 +0900

    feat Routers

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Main from "../pages/Main";
+import Detail from "../pages/post/Detail";
+import Update from "../pages/post/Update";
+import Profile from "../pages/user/Profile";
+import SignUp from "../pages/user/SignUp";
 
 function Router() {
-  return <div>Router</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Main />}></Route>
+      <Route path="/main" element={<Main />}></Route>
+      <Route path="/detail/:id" element={<Detail />}></Route>
+      <Route path="/update/:id" element={<Update />}></Route>
+      <Route path="/signup" element={<SignUp />}></Route>
+      <Route path="/profile" element={<Profile />}></Route>
+    </Routes>
+  );
 }
 
 export default Router;
