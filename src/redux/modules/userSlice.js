4ee53d400f1ed2d2208360c305add32294d6798b
DESKTOP-HCK0909\ggg71 <ggg7152@gmail.com>
2022-08-21 22:40:48 +0900

    feat/userSlice

---
+++ src/redux/modules/userSlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+const initialState = {
+  isLogin: localStorage.getItem("accessToken") ? true : false,
+};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    login: (state, action) => {
+      state.isLogin = true;
+    },
+    logout: (state, action) => {
+      state.isLogin = false;
+    },
+  },
+});
+
+export const { login, logout } = userSlice.actions;
+export default userSlice.reducer;
