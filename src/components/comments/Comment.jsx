75978b89e540bdfcf1e0162153b09acef7cd2272
DESKTOP-HCK0909\ggg71 <ggg7152@gmail.com>
2022-08-22 09:22:12 +0900

    feat image thumbnail image

--- src/components/comments/Comment.jsx
+++ src/components/comments/Comment.jsx
@@ -50,10 +50,7 @@ function Comment({ comment, post }) {
           <CommentUserProfile>
             {/* <User icon={faUser} /> */}
             <ImgBox>
-              <Image
-                class="profile"
-                src="http://t1.daumcdn.net/friends/prod/editor/dc8b3d02-a15a-4afa-a88b-989cf2a50476.jpg"
-              />
+              <Image class="profile" src={comment.imageUrl} />
             </ImgBox>
             <Writer> {comment.username}</Writer>
           </CommentUserProfile>
