e1bebc0a59acd592ce47c4fb389b35064d7b166f
DESKTOP-HCK0909\ggg71 <ggg7152@gmail.com>
2022-08-23 03:07:40 +0900

    fix api getPost

--- src/components/main/TwitList.jsx
+++ src/components/main/TwitList.jsx
@@ -1,23 +1,18 @@
-import React, { useEffect, Fragment } from "react";
+import React, { useEffect, Fragment, useState } from "react";
 import styled from "styled-components";
 import Twit from "./Twit";
 import { useSelector, useDispatch } from "react-redux";
 import { __getPost } from "../../redux/modules/postSlice";
 
-
-
 function TwitList() {
   const dispatch = useDispatch();
   const twits = useSelector((state) => state.post.list);
-
-  // console.log(twits)
-
-
+  const [page, setPage] = useState(0);
+  const pageSize = 5;
   useEffect(() => {
-    dispatch(__getPost());
+    dispatch(__getPost({ page: page, pageSize: pageSize }));
   }, [dispatch]);
 
-  
   return (
     <div>
       <TwitListBox>
@@ -38,7 +33,7 @@ function TwitList() {
 export default TwitList;
 
 const TwitListBox = styled.div`
-  width: 450px;
+  width: 100%;
   height: 700px;
   border-radius: 10px;
   border: 2px solid #eee;
