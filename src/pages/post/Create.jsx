928c06f12663a7058e6a2ca9e96dc5104ac5df01
DESKTOP-HCK0909\ggg71 <ggg7152@gmail.com>
2022-08-23 18:13:41 +0900

    fix styled css

--- src/pages/post/Create.jsx
+++ src/pages/post/Create.jsx
@@ -55,7 +55,6 @@ function Create() {
         <Input
           type="text"
           name="contents"
-          placeholder="내용"
           required
           {...register("contents")}
           placeholder="오늘은 어떤 일이 일어나고 있나요?"
@@ -119,7 +118,6 @@ const Input = styled.input`
   }
 `;
 
-
 const Form = styled.form`
   width: 100%;
   padding: 30px;
@@ -129,44 +127,15 @@ const Form = styled.form`
   align-items: center;
 `;
 
-const Input = styled.input`
-  box-sizing: border-box;
-  margin: 5px;
-  padding: 0;
-  font-variant: tabular-nums;
-  list-style: none;
-  font-feature-settings: "tnum", "tnum";
-  position: relative;
-  display: inline-block;
-  width: 100%;
-  min-width: 0;
-  padding: 4px 11px;
-  color: rgba(0, 0, 0, 0.85);
-  font-size: 14px;
-  line-height: 1.5715;
-  background-color: #fff;
-  background-image: none;
-  border: 1px solid #d9d9d9;
-  border-radius: 2px;
-  transition: all 0.3s;
-  &:hover {
-    border-color: #40a9ff;
-    border-right-width: 1px;
-  }
-  &:focus {
-    outline: none;
-  }
-`;
-
 const StButton = styled.button`
-  width : 70px;
-  height : 30px ;
-  background: #00ACEE;
-	border-radius: 20px;
-	color: #fff;
-	border-width: 0.5px;
-	border-style: solid;
-	border-color: #0075a2;
+  width: 70px;
+  height: 30px;
+  background: #00acee;
+  border-radius: 20px;
+  color: #fff;
+  border-width: 0.5px;
+  border-style: solid;
+  border-color: #0075a2;
   &:hover {
     border-color: #40a9ff;
     border-right-width: 1px;
@@ -180,17 +149,6 @@ const TwitBox = styled.div`
   justify-content: center;
   align-items: center;
   margin-top: 5px;
-`;
-
-`
-
-const TwitBox = styled.div`
-  display:flex;
-  margin-top : 5px;
-
-`
-const Icon = styled(FontAwesomeIcon)`
-  color: #5d5fef;
 `;
 
 const ImagePreview = styled.img`
